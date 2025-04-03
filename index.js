24927ba61e4bf0586c99350c40d3130cf2c0cd34
AmeyPSawant <63977498+AmeyPSawant@users.noreply.github.com>
2025-04-03 10:54:52 -0400

    Page inital loading only 30 articles

--- index.js
+++ index.js
@@ -3,12 +3,46 @@ const { chromium } = require("playwright");
 
 async function sortHackerNewsArticles() {
   // launch browser
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: false, executablePath: 'C://Users//ameyp//AppData//Local//ms-playwright//chromium-1084//chrome-win//chrome.exe' });
   const context = await browser.newContext();
   const page = await context.newPage();
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+  
+  // get the first 100 articles in the list of articles
+  const articles = await page.evaluate(() => {
+    const ageElements = Array.from(document.querySelectorAll(".age"));
+    return ageElements.slice(0,100).map((ele) => {
+      const title = ele.getAttribute("title");
+      return new Date(title).getTime();
+    });
+  });
+
+  // Validate we have exactly 100 articles
+  if (articles.length !== 100) {
+    throw new Error("Found " + articles.length + " articles. Only 100 articles are expected.");
+  }
+
+  // Check if the articles are sorted from newest to oldest
+  let isValid = true;
+  for(i=0; i < articles.length - 1; i++) {
+    if (articles[i] < articles[i + 1]) {
+      isValid = false;
+      break;
+    }
+  }
+
+  // Print the result
+  if(isValid) {
+    console.log("Articles are sorted from newest to oldest");
+  }
+  else {
+    console.log("Articles are NOT sorted from newest to oldest");
+  }
+
+  // Close browser
+  await browser.close();
 }
 
 (async () => {
